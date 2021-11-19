733bc695ddc89e90a92b1ba88f17352f9755bcb6
Jerry <ordinary9843@gmail.com>
2021-11-19 15:43:47 +0800

    [feat] Modify ghostscript in windows binary path

--- tests/GhostscriptTest.php
+++ tests/GhostscriptTest.php
@@ -22,7 +22,7 @@ class GhostscriptTest extends TestCase
         $isWindows = (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN');
 
         if ($isWindows === true) {
-            $binPath = 'C:\Program Files\gs\gs9.55.0\gswin64c.exe';
+            $binPath = 'C:\gs\gs9.55.0\bin\gswin64c.exe';
             $tmpPath = 'C:\Windows\TEMP';
         } else {
             $binPath = '/usr/bin/gs';
