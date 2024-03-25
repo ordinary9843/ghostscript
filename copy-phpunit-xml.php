57e05d279ad675297d5985793582d5fe89a1c4b0
Jerry Chen <ordinary9843@gmail.com>
2024-03-25 21:09:29 +0800

    [feat] handle phpunit version

--- copy-phpunit-xml.php
+++ copy-phpunit-xml.php
@@ -9,11 +9,11 @@ if (!$matches) {
 
 $phpunitVersion = current($matches);
 if (version_compare($phpunitVersion, '9.0', '<')) {
-    @copy('phpunit-7.0.0.xml', 'phpunit.xml');
+    @copy('phpunit-7.xml', 'phpunit.xml');
 } else {
     if ((PHP_MAJOR_VERSION === 7 && in_array(PHP_MINOR_VERSION, [3, 4])) || (PHP_MAJOR_VERSION === 8 && PHP_MINOR_VERSION === 0)) {
-        @copy('phpunit-9.0.0-coverage.xml', 'phpunit.xml');
+        @copy('phpunit-9-coverage.xml', 'phpunit.xml');
     } else {
-        @copy('phpunit-9.0.0-source.xml', 'phpunit.xml');
+        @copy('phpunit-9-source.xml', 'phpunit.xml');
     }
 }
