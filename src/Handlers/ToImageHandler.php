4e669b23d8007e03ca3b22f16c8389e1e98c0e39
Jerry Chen <ordinary9843@gmail.com>
2024-03-27 16:17:31 +0800

    [refactor] to image handler & tests

--- src/Handlers/ToImageHandler.php
+++ src/Handlers/ToImageHandler.php
@@ -8,11 +8,20 @@ use Ordinary9843\Constants\ToImageConstant;
 use Ordinary9843\Exceptions\HandlerException;
 use Ordinary9843\Exceptions\InvalidException;
 use Ordinary9843\Interfaces\HandlerInterface;
+use Ordinary9843\Handlers\GetTotalPagesHandler;
 
 class ToImageHandler extends BaseHandler implements HandlerInterface
 {
     /** @var array */
     protected $argumentsMapping = ['file', 'path', 'type'];
+
+    /** @var GetTotalPagesHandler */
+    protected $getTotalPagesHandler = null;
+
+    public function __construct()
+    {
+        $this->getTotalPagesHandler = new GetTotalPagesHandler();
+    }
 
     /**
      * @param array ...$arguments
@@ -31,13 +40,9 @@ class ToImageHandler extends BaseHandler implements HandlerInterface
             $file = PathHelper::convertPathSeparator($arguments['file']);
             $path = PathHelper::convertPathSeparator($arguments['path']);
             $type = PathHelper::convertPathSeparator($arguments['type'] ?? ToImageConstant::TYPE_JPEG);
-            $totalPage = $this->getPdfTotalPage($file);
-            if ($totalPage < 1) {
-                throw new HandlerException('Failed to read the total number of pages in "' . $file . '".', HandlerException::CODE_EXECUTE);
-            }
-
+            $totalPages = $this->getTotalPagesHandler->execute($file);
             (!$this->isDir($path)) && mkdir($path, 0755);
-            $imageFormatPath = ($totalPage > 1) ? '/image_%d.' . $type : '/' . pathinfo($file, PATHINFO_FILENAME) . '.' . $type;
+            $imageFormatPath = ($totalPages > 1) ? '/image_%d.' . $type : '/' . pathinfo($file, PATHINFO_FILENAME) . '.' . $type;
             $output = shell_exec($this->optionsToCommand($this->getBinPath() . ' -dQUIET -dNOPAUSE -dBATCH -sDEVICE=' . ToImageConstant::TYPE_JPEG . ' -r300 -sOutputFile=' . escapeshellarg(PathHelper::convertPathSeparator($path . $imageFormatPath)) . ' ' . escapeshellarg(PathHelper::convertPathSeparator($this->convertToTmpFile($file)))));
             if ($output) {
                 throw new HandlerException('Failed to convert "' . $file . '", because ' . $output . '.', HandlerException::CODE_EXECUTE);
@@ -45,7 +50,7 @@ class ToImageHandler extends BaseHandler implements HandlerInterface
 
             return array_map(function ($i) use ($path, $imageFormatPath) {
                 return $path . sprintf($imageFormatPath, $i + 1);
-            }, range(0, $totalPage - 1));
+            }, range(0, $totalPages - 1));
         } catch (BaseException $exception) {
             throw new HandlerException($exception->getMessage(), HandlerException::CODE_EXECUTE, [
                 'arguments' => $arguments
--- tests/Handlers/ToImageHandlerTest.php
+++ tests/Handlers/ToImageHandlerTest.php
@@ -2,92 +2,64 @@
 
 namespace Tests\Handlers;
 
-use Tests\TestCase;
-use Ordinary9843\Configs\Config;
+use Tests\BaseTestCase;
 use Ordinary9843\Handlers\ToImageHandler;
-use Ordinary9843\Constants\MessageConstant;
 use Ordinary9843\Constants\ToImageConstant;
+use Ordinary9843\Exceptions\HandlerException;
 
-class ToImageHandlerTest extends TestCase
+class ToImageHandlerTest extends BaseTestCase
 {
     /**
      * @return void
      */
-    public function testExecuteWithExistFileShouldSucceed(): void
+    public function testExecuteShouldSucceed(): void
     {
-        $toImageHandler = new ToImageHandler();
-        $toImageHandler->setBinPath($this->getEnv('GS_BIN_PATH'));
-        $this->assertCount(3, $toImageHandler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'));
-        $this->assertFalse($toImageHandler->hasMessages(MessageConstant::TYPE_ERROR));
+        $handler = new ToImageHandler();
+        $handler->setBinPath($this->getEnv('GS_BIN_PATH'));
+        $this->assertCount(3, $handler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'));
     }
 
     /**
      * @return void
      */
-    public function testExecuteWithExistFileAndTypeEqualJpegShouldSucceed(): void
+    public function testExecuteWhenTypeEqualJpegShouldSucceed(): void
     {
-        $toImageHandler = new ToImageHandler();
-        $toImageHandler->setBinPath($this->getEnv('GS_BIN_PATH'));
-        $this->assertCount(3, $toImageHandler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'), ToImageConstant::TYPE_JPEG);
-        $this->assertFalse($toImageHandler->hasMessages(MessageConstant::TYPE_ERROR));
+        $handler = new ToImageHandler();
+        $handler->setBinPath($this->getEnv('GS_BIN_PATH'));
+        $this->assertCount(3, $handler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'), ToImageConstant::TYPE_JPEG);
     }
 
     /**
      * @return void
      */
-    public function testExecuteWithExistFileAndTypeEqualPngShouldSucceed(): void
+    public function testExecuteWhenTypeEqualPngShouldSucceed(): void
     {
-        $toImageHandler = new ToImageHandler();
-        $toImageHandler->setBinPath($this->getEnv('GS_BIN_PATH'));
-        $this->assertCount(3, $toImageHandler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'), ToImageConstant::TYPE_PNG);
-        $this->assertFalse($toImageHandler->hasMessages(MessageConstant::TYPE_ERROR));
+        $handler = new ToImageHandler();
+        $handler->setBinPath($this->getEnv('GS_BIN_PATH'));
+        $this->assertCount(3, $handler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'), ToImageConstant::TYPE_PNG);
     }
 
     /**
      * @return void
      */
-    public function testExecuteWithExistFileShouldSucceedWhenFilenameHasChinese(): void
+    public function testExecuteWhenFilenameHasChineseShouldSucceed(): void
     {
-        $toImageHandler = new ToImageHandler();
-        $toImageHandler->setBinPath($this->getEnv('GS_BIN_PATH'));
-        $this->assertCount(1, $toImageHandler->execute(dirname(__DIR__, 2) . '/files/gs_ -test/中文.pdf', '/tmp/mock/files'));
-        $this->assertFalse($toImageHandler->hasMessages(MessageConstant::TYPE_ERROR));
+        $handler = new ToImageHandler();
+        $handler->setBinPath($this->getEnv('GS_BIN_PATH'));
+        $this->assertCount(1, $handler->execute(dirname(__DIR__, 2) . '/files/gs_ -test/中文.pdf', '/tmp/mock/files'));
     }
 
     /**
      * @return void
      */
-    public function testExecuteWithNotExistFileShouldReturnErrorMessage(): void
+    public function testExecuteFailedShouldThrowHandlerException(): void
     {
-        $methods = ['getPdfTotalPage', 'getConfig'];
-        if ($this->isPhpUnitVersionInRange(self::PHPUNIT_MIN_VERSION, self::PHPUNIT_VERSION_9)) {
-            $toImageHandler = $this->getMockBuilder(ToImageHandler::class)
-                ->setMethods($methods)
-                ->getMock();
-        } else {
-            $toImageHandler = $this->getMockBuilder(ToImageHandler::class)
-                ->onlyMethods($methods)
-                ->getMock();
-        }
-
-        $toImageHandler->method('getConfig')->willReturn(new Config(['binPath' => $this->getEnv('GS_BIN_PATH')]));
-        $toImageHandler->method('getPdfTotalPage')->willReturn(0);
-        $this->assertCount(0, $toImageHandler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files'));
-        $this->assertTrue($toImageHandler->hasMessages(MessageConstant::TYPE_ERROR));
-    }
-
-    /**
-     * @return void
-     */
-    public function testExecuteFailedShouldReturnErrorMessage(): void
-    {
-        $toImageHandler = new ToImageHandler(new Config([
-            'binPath' => $this->getEnv('GS_BIN_PATH')
-        ]));
-        $toImageHandler->setOptions([
+        $this->expectException(HandlerException::class);
+        $handler = new ToImageHandler();
+        $handler->setBinPath($this->getEnv('GS_BIN_PATH'));
+        $handler->setOptions([
             'test' => true
         ]);
-        $toImageHandler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files');
-        $this->assertTrue($toImageHandler->hasMessages(MessageConstant::TYPE_ERROR));
+        $handler->execute(dirname(__DIR__, 2) . '/files/to-image/test.pdf', '/tmp/mock/files');
     }
 }
