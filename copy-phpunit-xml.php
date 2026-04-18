883fdca53677f0c7b6eea68f9eda04ac118e735c
Jerry Chen <ordinary9843@gmail.com>
2026-04-18 10:03:54 +0800

    [chore] cap phpunit to <14.0, simplify version detection in copy-phpunit-xml

--- copy-phpunit-xml.php
+++ copy-phpunit-xml.php
@@ -10,10 +10,8 @@ if (!$matches) {
 $phpunitVersion = current($matches);
 if (version_compare($phpunitVersion, '9.0', '<')) {
     @copy('phpunit-7.xml', 'phpunit.xml');
+} elseif (version_compare($phpunitVersion, '10.0', '<')) {
+    @copy('phpunit-9.xml', 'phpunit.xml');
 } else {
-    if ((PHP_MAJOR_VERSION === 7 && in_array(PHP_MINOR_VERSION, [3, 4])) || (PHP_MAJOR_VERSION === 8 && PHP_MINOR_VERSION === 0)) {
-        @copy('phpunit-9.xml', 'phpunit.xml');
-    } else {
-        @copy('phpunit-10.xml', 'phpunit.xml');
-    }
+    @copy('phpunit-10.xml', 'phpunit.xml');
 }
